e5a076cbd1a6785c3f2bb9638b0d316fae969af9
chackhangun <chleorjs9@gmail.com>
2022-01-25 23:09:39 +0900

    add animation

--- src/component/Dg.js
+++ src/component/Dg.js
@@ -1,38 +1,54 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import { useSpring, animated, config } from 'react-spring'
 import './Dg.css'
 import './Ipad.style.module.css'
+import AOS from "aos"
+import 'aos/dist/aos.css';
 import Ipad from './Ipad.tsx'
 
+
 const Dg = () => {
+    let boxsytle = {
+        width: '40%',
+        height: '200px',
+        fontSize: '30px',
+        lineHeight: '200px',
+        blackgroud: 'black',
+        color: 'white',
+        textAlign: 'center'
+    }
+    useEffect(()=> {
+        AOS.init({
+            duration: 1000});
+    });
     return(
         <>
-            <div className='small_title1'>
+            <div className='small_title1' data-aos='fade-up'>
                     주당 80-100시간의 <br/> 집중 개발.
             </div>
             
-            <div className = "description1">
+            <div className = "description1" data-aos='fade-up'>
                 몰입캠프에서는 관심있는 프로젝트 주제를 디자인하고 프로토<br/> 타입으로의 구현까지 개발의 전 과정을 경험할 수 있습니다.<br/> 약 4.5주간 4번의 프로젝트를 진행하며 하루종일 개발에  <br/> 몰두하는 즐거움을 느껴보세요. 
             </div>
-            <div className = "small_title2">
+            <div className = "small_title2" data-aos='fade-up'>
                 매일매일 실력향상
             </div>
-            <div className = 'description2'>
+            <div className = 'description2' data-aos='fade-up'>
                 공통프로젝트를 통해 개발환경과 체계에 대한 이해도를 높이고, <br/> 자유프로젝트에서 App,Web,게임,AI 등 원하는 분야와 기술스택을 선택해 <br/> 다양한 경험과 실력을 쌓아보세요. 2~3명으로 구성된 팀으로 프로젝트를 <br/> 수행하며 협업개발을 배우고 경험할 수 있습니다. <br/> 매일 정기적인 스크럼과 매주 프로젝트 발표를 통해 <br/> 다른팀들과 시행착오를 나누며 교류해보세요.
             </div>
-            <div className = 'description2'>
+            <div className = 'description2' data-aos='fade-up'>
                 <img src = "png/temp.png" alt = "기술스택"></img>
             </div>
-            <div className = 'temp2'>
+            <div className = 'temp2' data-aos='zoom-in' data-aos-offset='600'>
                 <img src = "png/temp3.png" alt = "아이콘"></img>
             </div>
-            <div className = 'small_title3'>
+            <div className = 'small_title3' data-aos='zoom-in'>
                 강력한 <br/> 네트워킹
             </div>
-            <div>
+            <div data-aos='zoom-in' data-aos-offset='300'>
                 <img src = "png/temp2.png" alt = "아이콘"></img>
             </div>
-            <div className='description3'>
+            <div className='description3' data-aos='fade-up' data-aos-offset='100'>
                 스타트업에서 네트워크는 가장 강력한 힘입니다. <br/> 
                 몰입캠프에서 나만의 네트워크를 구축해보세요. <br/> 
                 몰입캠프는 KAIST와 MOU 체결이 되어있는 국내 최고의 대학교 학생들 80여명을 이어줍니다. <br/> 
@@ -41,7 +57,7 @@ const Dg = () => {
                 협업, 스크럼 뿐만 아니라 몰입캠프에서 지원해주는 야식쿠폰과 회식을 통해 <br/> 
                 당신의 소중한 동료들을 늘려보세요. 
             </div>
-            <div className = 'Ipad'>
+            <div className = 'Ipad' >
                 <img className = 'Ipad' src = "png/ipad.png"></img>
                 <div className = 'image'>
                     <Ipad></Ipad>
@@ -49,11 +65,11 @@ const Dg = () => {
             </div>
 
             
-            <div className = 'small_title4'>
+            <div className = 'small_title4' data-aos='fade-up'>
                 든든한 <br/> 운영진과 후원사
             </div>
-            <div className='bigbox'>
-                <div className ='container1'>
+            <div className='bigbox' data-aos='fade-up'>
+                <div className ='container1' data-aos='fade-up'>
                     <img className = 'prof' src = "png/BK.png"></img>
                     <div>
                         <div className = 'explain1' style={{display: "inline"}}>
@@ -95,7 +111,7 @@ const Dg = () => {
                     </ul>
                 </div>
 
-                <div className='container2'>
+                <div className='container2' data-aos='fade-up'>
                     <img className = 'prof' src = "png/SY.png"></img>
                     <div>
                         <div className = 'explain1' style={{display: "inline"}}>
@@ -139,23 +155,20 @@ const Dg = () => {
                 <img src = "png/mad.png"></img>
             </div>
 
-            <div className = "startup_start">
+            <div className = "startup_start" data-aos='fade-up'>
                 반가워요 선배님들. <br/> 
                 세상을 바꿀 획기적인 스타트업 <br/> 
                 대표님들을 소개합니다.
             </div>
-            <div>
+            <div data-aos='fade-up' data-aos-offset='400'>
                 <img className = "startup_img" src= "png/startup_logo.png"></img> 
             </div>
 
-            <div className = "startup_description">
-                여러 대표님들과의 대화를 통해 <br/> 스타트업의 희로애락을 미리 체험해보세요.
+            <div className = "startup_description" data-aos='fade-up'>
+                캠프 기간동안 여러 대표님들의 강연을 통해 <br/> 스타트업의 희로애락을 미리 체험해보세요.
             </div>
 
         </>
-
-
-
         
     )
 }
